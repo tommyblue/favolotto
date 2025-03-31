49e731a40d0d01a6c63a01f23eb4fc821ed1fad7
Tommaso Visconti <tommaso.visconti@gmail.com>
2025-03-31 22:46:16 +0200

    manage nfc driver with config

--- backend/nfc.go
+++ backend/nfc.go
@@ -2,6 +2,7 @@ package favolotto
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"time"
 
@@ -20,7 +21,7 @@ type Nfc struct {
 	in     chan<- string
 }
 
-func NewNFC(driverName string, in chan<- string) *Nfc {
+func NewNFC(driverName string, in chan<- string) (*Nfc, error) {
 	var driver NfcDriver
 	var err error
 	switch driverName {
@@ -28,17 +29,20 @@ func NewNFC(driverName string, in chan<- string) *Nfc {
 		driver, err = pn7150.New()
 	case "pn532":
 		driver, err = pn532.New()
+	default:
+		log.Printf("Unknown NFC driver: %s\n", driverName)
+		return nil, fmt.Errorf("unknown NFC driver: %s", driverName)
 	}
 
 	if err != nil {
 		log.Printf("Error while init nfc device\n", err)
-		return nil
+		return nil, err
 	}
 
 	return &Nfc{
 		in:     in,
 		driver: driver,
-	}
+	}, nil
 }
 
 func (n *Nfc) Run(ctx context.Context) {
