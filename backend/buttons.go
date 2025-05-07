183849425690459a3ae2273d8febabd3e270d5df
Daniele Basile <asterix@develer.com>
2025-05-08 00:02:45 +0200

    use same gpiod for all modules

--- backend/buttons.go
+++ backend/buttons.go
@@ -6,51 +6,37 @@ import (
 	"log"
 	"time"
 
-	"periph.io/x/conn/v3/gpio"
-	"periph.io/x/host/v3"
-	"periph.io/x/host/v3/rpi"
+	"github.com/warthog618/go-gpiocdev"
 )
 
-var buttonsPin = map[string]gpio.PinIO{
-	"btn1": rpi.P1_11,
-	"btn2": rpi.P1_32,
-	"btn3": rpi.P1_33,
-}
-
-var messages = map[string]string{
-	"btn1": Volume,
-	"btn2": Pause,
-	"btn3": Volume,
+var buttonsMap = map[int]string{
+	22: Pause,  // "btnLeft":
+	23: Resume, // "btnMid":
+	24: Stop,   // "btnRight":
 }
 
 type Buttons struct {
-	in  chan<- string
-	btn map[string]*gpio.PinIO
+	in      chan<- string
+	pins    *gpiocdev.Lines
+	offsets []int
 }
 
 func NewButton(in chan<- string) (*Buttons, error) {
+	offsets := []int{}
+	for x := range buttonsMap {
+		offsets = append(offsets, x)
+	}
+	pins, err := gpiocdev.RequestLines("/dev/gpiochip0", offsets, gpiocdev.AsInput)
+	if err != nil {
+		fmt.Printf("Requesting lines returned error: %s\n", err)
+	}
+	//defer pins.Close()
 
 	return &Buttons{
-		in:  in,
-		btn: make(map[string]*gpio.PinIO),
+		in:      in,
+		pins:    pins,
+		offsets: offsets,
 	}, nil
-}
-
-func (b *Buttons) init() error {
-	if _, err := host.Init(); err != nil {
-		return fmt.Errorf("error during initialization: %v", err)
-	}
-
-	for label, pin := range buttonsPin {
-		// configure as input with internal pull-up
-		if err := pin.In(gpio.PullUp, gpio.BothEdges); err != nil {
-			return fmt.Errorf("error configuring GPIO: %v", err)
-		}
-
-		b.btn[label] = &pin
-	}
-
-	return nil
 }
 
 func (b *Buttons) Run(ctx context.Context) {
@@ -60,13 +46,9 @@ func (b *Buttons) Run(ctx context.Context) {
 		return
 	}
 
-	if err := b.init(); err != nil {
-		log.Fatalf("error initializing button: %v", err)
-	}
-
-	pressed := make(map[string]bool)
-	for label := range b.btn {
-		pressed[label] = false
+	pressed := []bool{}
+	for _ = range b.offsets {
+		pressed = append(pressed, false)
 	}
 
 	for {
@@ -75,15 +57,22 @@ func (b *Buttons) Run(ctx context.Context) {
 			log.Println("button context done")
 			return
 		default:
-			for label, pin := range b.btn {
-				if (*pin).Read() == gpio.Low {
-					pressed[label] = true
-				} else if pressed[label] {
-					pressed[label] = false
-					b.in <- messages[label]
+			time.Sleep(100 * time.Millisecond) // debounce time
+
+			readValues := []int{0, 0, 0}
+			err := b.pins.Values(readValues)
+			if err != nil {
+				continue
+			}
+
+			for idx := range b.offsets {
+				if readValues[idx] == 0 {
+					pressed[idx] = true
+				} else if pressed[idx] {
+					pressed[idx] = false
+					b.in <- buttonsMap[b.offsets[idx]]
 				}
 			}
-			time.Sleep(200 * time.Millisecond) // debounce time
 		}
 	}
 
